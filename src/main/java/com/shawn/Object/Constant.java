94b2ef3678fbacaf9cf2f6429d0f25dcb8c8d4dd
Shawn <c.shangfei@gmail.com>
2013-01-20 14:41:56 +0800

    new test

---
+++ src/main/java/com/shawn/Object/Constant.java
@@ -0,0 +1,26 @@
+package com.shawn.Object;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: caocao024
+ * Date: 13-1-17
+ * Time: PM12:38
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constant {
+    static final  int number1 = 5;
+
+    static final  int number2 = 6;
+
+    static int number3 = 5;
+
+    static int number4= 6;
+
+    public static void main(String[ ] args) {
+
+        int product1 = number1 * number2;         //line A
+
+        int product2 = number3 * number4;         //line B
+
+    }
+}
