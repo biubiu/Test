5f19a3db338371987282aeffcba34cbebcb80f74
biubiu <c.shangfei@gmail.com>
2013-10-18 18:10:44 +0800

    digest

--- src/main/java/com/shawn/nio/DigestTest.java
+++ src/main/java/com/shawn/nio/DigestTest.java
@@ -1,11 +1,35 @@
 package com.shawn.nio;
 
+import sun.security.util.BigInt;
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.math.BigInteger;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class DigestTest {
-
-
-  /*  private static BigInteger digest(){
-
+    public static void main(String args[]) throws IOException, NoSuchAlgorithmException {
+        digest("http://www.oreilly.com");
     }
-*/}
+
+   private static String digest(String urlAdd) throws IOException, NoSuchAlgorithmException {
+       URL url = new URL(urlAdd);
+       InputStream inputStream = url.openStream();
+       MessageDigest digest = MessageDigest.getInstance("MD5");
+       byte[] data = new byte[128];
+        while(true){
+            int byteRead = inputStream.read(data);
+            if(byteRead <0 ) break;
+            System.out.println("byte reads: " + byteRead);
+            digest.update(data,0,byteRead);
+        }
+       byte[] result = digest.digest();
+       for (int i=0; i<result.length;i++){
+           System.out.print(result[i]+"\t");
+       }
+       System.out.println("\n" +new BigInteger(result));
+       return new String(result);
+    }
+}
