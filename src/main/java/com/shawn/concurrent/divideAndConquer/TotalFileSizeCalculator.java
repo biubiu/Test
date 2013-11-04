ab68dbdf297da7c90d55892dfb9fcb5eb92e1895
biubiu <c.shangfei@gmail.com>
2013-11-04 09:39:26 +0800

    file size example

---
+++ src/main/java/com/shawn/concurrent/divideAndConquer/TotalFileSizeCalculator.java
@@ -0,0 +1,223 @@
+package com.shawn.concurrent.divideAndConquer;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.concurrent.atomic.AtomicLong;
+
+import com.google.common.collect.Lists;
+
+public class TotalFileSizeCalculator {
+	public static void main(final String[] args) throws Exception {
+		final long start = System.nanoTime();
+		// final long total = new
+		// TotalFileSizeSequential().getTotalSizeOfFilesInDir(new
+		// File(args[0]));
+		// final long total = new
+		// ConcurrentTotalFileSize().getTotalSizeOfFilesInDir(new
+		// File(args[0]));
+		//final long total = new ConcurrentTotalFileSizeWQueue().getTotalSizeOfFile(new File(args[0]));
+		final long total =ForkRecursiveFileSize.forkJoin.invoke(new ForkRecursiveFileSize.FileSizeFinder(new File(args[0])));
+		final long end = System.nanoTime();
+		System.out.println("Total Size: " + total);
+		System.out.println("Time taken: " + (end - start) / 1.0e9);
+	}
+}
+
+class TotalFileSizeSequential {
+	public long getTotalSizeOfFilesInDir(final File file) {
+		if (file.isFile())
+			return file.length();
+		final File[] children = file.listFiles();
+		long total = 0;
+		if (children != null)
+			for (final File child : children)
+				total += getTotalSizeOfFilesInDir(child);
+		return total;
+	}
+}
+
+class NaivelyConcurrentTotalFileSize {
+	public long getTotalSizeOfFilesInDir(final ExecutorService service, final File file) throws InterruptedException,
+			ExecutionException, TimeoutException {
+		if (file.isFile())
+			return file.length();
+		long total = 0;
+		final File[] children = file.listFiles();
+		if (children != null) {
+			final List<Future<Long>> partialTotalFutures = new ArrayList<Future<Long>>();
+			for (final File child : children) {
+				partialTotalFutures.add(service.submit(new Callable<Long>() {
+					public Long call() throws InterruptedException, ExecutionException, TimeoutException {
+						return getTotalSizeOfFilesInDir(service, child);
+					}
+				}));
+			}
+			for (final Future<Long> partialTotalFuture : partialTotalFutures)
+				total += partialTotalFuture.get(100, TimeUnit.SECONDS);
+		}
+		return total;
+	}
+}
+
+class ConcurrentTotalFileSize {
+
+	class SubDirectoriesAndSize {
+		final public long size;
+		final public List<File> subDirectories;
+
+		public SubDirectoriesAndSize(final long totalSize, final List<File> theSubDirs) {
+			size = totalSize;
+			subDirectories = Collections.unmodifiableList(theSubDirs);
+		}
+	}
+
+	private SubDirectoriesAndSize getTotalAndSubDirs(final File file) {
+		long totalSize = 0;
+		final List<File> subDirs = new ArrayList<>();
+		if (file.isDirectory()) {
+			final File[] children = file.listFiles();
+			if (children != null) {
+				for (final File child : children) {
+					if (child.isFile()) {
+						totalSize += child.length();
+					} else {
+						subDirs.add(child);
+					}
+				}
+			}
+		}
+		return new SubDirectoriesAndSize(totalSize, subDirs);
+	}
+
+	public long getTotalSizeOfFilesInDir(final File file) throws InterruptedException, ExecutionException, TimeoutException {
+		final ExecutorService executor = Executors.newFixedThreadPool(100);
+		try {
+			long total = 0;
+			final List<File> directories = Lists.newArrayList();
+			directories.add(file);
+			while (!directories.isEmpty()) {
+				final List<Future<SubDirectoriesAndSize>> partialResult = Lists.newArrayList();
+				for (final File directory : directories) {
+					partialResult.add(executor.submit(new Callable<SubDirectoriesAndSize>() {
+						public SubDirectoriesAndSize call() {
+							return getTotalAndSubDirs(directory);
+						}
+					}));
+				}
+				directories.clear();
+				for (final Future<SubDirectoriesAndSize> partialResultFuture : partialResult) {
+					final SubDirectoriesAndSize subDirectoriesAndSize = partialResultFuture.get(100, TimeUnit.SECONDS);
+					directories.addAll(subDirectoriesAndSize.subDirectories);
+					total += subDirectoriesAndSize.size;
+				}
+			}
+			return total;
+		} finally {
+			executor.shutdown();
+		}
+	}
+}
+
+class ConcurrentTotalFileSizeWQueue {
+	private ExecutorService service;
+	final private BlockingQueue<Long> fileSizes = new ArrayBlockingQueue<Long>(500);
+	final AtomicLong pendingFileVisits = new AtomicLong();
+
+	private void startExploreDir(final File file) {
+		pendingFileVisits.incrementAndGet();
+		service.execute(new Runnable() {
+			public void run() {
+				exploreDir(file);
+			}
+		});
+	}
+
+	private void exploreDir(File file) {
+		long fileSize = 0;
+		if (file.isFile())
+			fileSize = file.length();
+		else {
+			final File[] children = file.listFiles();
+			if (children != null)
+				for (final File child : children) {
+					if (child.isFile())
+						fileSize += child.length();
+					else {
+						startExploreDir(child);
+					}
+				}
+		}
+		try {
+			fileSizes.put(fileSize);
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+		pendingFileVisits.decrementAndGet();
+	}
+
+	public long getTotalSizeOfFile(final File file) throws InterruptedException {
+		service = Executors.newFixedThreadPool(100);
+		try {
+			startExploreDir(file);
+			long totalSize = 0;
+			while (pendingFileVisits.get() > 0 || fileSizes.size() > 0) {
+				final Long size = fileSizes.poll(10, TimeUnit.SECONDS);
+				totalSize += size;
+			}
+			return totalSize;
+		} finally {
+			service.shutdown();
+		}
+	}
+}
+
+class ForkRecursiveFileSize {
+	public final static ForkJoinPool forkJoin = new ForkJoinPool();
+
+	public static class FileSizeFinder extends RecursiveTask<Long> {
+		final File file;
+
+		public FileSizeFinder(final File theFile) {
+			file = theFile;
+		}
+
+		@Override
+		protected Long compute() {
+			long size = 0;
+			if(file.isFile()){
+				size = file.length();
+			}else{
+				final File[] children = file.listFiles();
+				if(children!=null){
+					List<ForkJoinTask<Long>> tasks = Lists.newArrayList();
+					for(final File child:children){
+						if(child.isFile()){
+							size+=child.length();
+						}else {
+							tasks.add(new FileSizeFinder(child));
+						}
+					}
+					for(final ForkJoinTask<Long> task: invokeAll(tasks)){
+						size+=task.join();
+					}
+				}
+			}
+			return size;
+		}
+
+	}
+}
